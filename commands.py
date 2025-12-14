459a3f31a4556b4b99b9ec4315f180cfdef2bace
akiltrebreg <akiltrebreg@yandex.ru>
2025-12-14 15:08:19 +0300

    Change commands.py

--- commands.py
+++ commands.py
@@ -3,12 +3,13 @@ import fire
 from bee_vs_wasp.train import train as hydra_train
 
 
-def train() -> None:
-    hydra_train()
+class CLI:
+    def train(self) -> None:
+        hydra_train()
 
 
 def main() -> None:
-    fire.Fire({"train": train})
+    fire.Fire(CLI)
 
 
 if __name__ == "__main__":
